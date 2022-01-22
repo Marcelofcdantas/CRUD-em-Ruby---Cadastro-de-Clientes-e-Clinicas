aba852b1faf6420de97383282ee4291fec834294
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2022-01-22 18:17:18 -0300

    adicionadas validaçoes nos campos de entrada

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,2 +1,12 @@
 class Cliente < ApplicationRecord
+    validates :nome, length: {minimum:3}
+    validates :rg, confirmation: true
+    validates :cpf, length: {minimum:14, maximum:14}
+    validates :estado_civil, presence: true
+    validates :rua, presence: true
+    validates :numero, numericality: { only_integer: true }
+    validates :bairro, presence: true
+    validates :cidade, presence: true
+    validates :estado, presence: true
+    validates :cep, length: {minimum:9, maximum:9}
 end
--- app/models/clinica.rb
+++ app/models/clinica.rb
@@ -1,2 +1,11 @@
 class Clinica < ApplicationRecord
+    validates :nome_fantasia, length: {minimum:3}
+    validates :razao_social, confirmation: true
+    validates :cnpj, length: {minimum:18, maximum:18}
+    validates :rua, presence: true
+    validates :numero, numericality: { only_integer: true }
+    validates :bairro, presence: true
+    validates :cidade, presence: true
+    validates :estado, presence: true
+    validates :cep, length: {minimum:9, maximum:9}
 end
